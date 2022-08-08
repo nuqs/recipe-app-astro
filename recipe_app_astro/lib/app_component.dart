66b57a247187dee3f5da5b59553a52d19f92a97a
nuqman <nuqman.manja@shoppertise.com>
2022-08-08 20:47:34 +0800

    Clean code

--- recipe_app_astro/lib/app_component.dart
+++ recipe_app_astro/lib/app_component.dart
@@ -24,7 +24,6 @@ class AppComponent extends StatefulWidget {
 class _AppComponentState extends State<AppComponent> {
   @override
   void initState() {
-    print("AppComponent.initState()");
     super.initState();
   }
 
